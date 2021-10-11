eb3f972516a576aafe424977e90b13111ba9581c
AlefAlencar <89526613+AlefAlencar@users.noreply.github.com>
2021-10-11 00:08:28 -0300

    algoritmos_warmup

--- hackerrank_challenges/s13_regex_and_parsing.py
+++ hackerrank_challenges/s13_regex_and_parsing.py
@@ -2,6 +2,35 @@
 DESAFIOS HACKERRANK
 13. Regex and Parsing (expressões regulares e análise)
 """
+# Exemplos
+import re
+
+'''# group, groups, groupdict
+import re
+m = re.match(
+    r'(\w+)@(\w+)\.(\w+)', 'username@hackerrank.com'
+)
+print(m.group(0), '\n',  # correspondência inteira
+      m.group(1), '\n',  # primeiro subgrupo
+      m.group(2), '\n',
+      m.group(3), '\n',
+      m.group(1, 3))  # tupla de multiplos subgrupos
+print(m.groups())  # tupla de todos as correspondências
+n = re.match(r'(?P<user>\w+)@(?P<website>\w+)\.(?P<extension>\w+)',
+             'myname@hackerrank.com')
+print(n.groupdict())
+a = re.finditer(r'\w', 'http://www.hackerrank.com/')
+b = list(map(lambda x: x.group(), re.finditer(r'\w', 'http://www.hackerrank.com/')))
+print(a, '\n',
+      b)'''
+
+s = 'abc123abc456'
+f = re.search(r'\d+', s)
+a = re.findall(r'\d+', s)
+print(a)
+# print(a.end(), '\n',
+#       a.start())
+# '''
 
 # 1/17 Detetc Floating Point Number {1ª 9/18f 0pt}
 '''# {1ª 9/18f, 2ª adicionado try float(S)}
@@ -28,9 +57,33 @@ for _ in range(int(input())):  # resumido:
 +4.10
 '''
 
-# 2/17 Re.split()
+# 2/17 re.split()
 '''import re
 regex_pattern = r",."	 # Do not delete 'r'. # bem simples
 print("\n".join(re.split(regex_pattern, input())))  # '''
 
-# 3/17 
+# 3/17 group(), groups() & groudict() {1ª: 2/6f 0pt}
+# encontre a primeira repeticao consecutiva de um caractere alfanumérico
+'''import re
+s = input()
+m = re.search(r'([a-zA-Z0-9])\1+', s)
+print(m.group()[0])  # '''
+
+# 4/17 re.findall() & re.finditer()
+'''import re
+s = input()
+c = 'qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM '
+v = 'aeiouAEIOU'
+res = re.findall(r'(?<=[%s])([%s]{2,})[%s]' % (c, v, c), s)
+# ?<= "precedido"
+# {2,} "dois ou mais"
+# %
+print(res)
+if not res:
+    print(-1)
+else:
+    for i in res:  # OUTPUT
+        if i:
+            print(i)  # '''
+
+# 5/17 re.start() & re.end()
---
+++ hackerrank_challenges/solving_problems/s01_warmup.py
@@ -0,0 +1,128 @@
+"""
+DESAFIOS HACKERRANK
+Algoritmos:
+1. Warm-up
+"""
+
+
+# 1/10 Solve Me First
+'''def solveMeFirst(a,b):
+    # Hint: Type return a+b below
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)  # '''
+
+# 2/10 Simple Array Sum
+'''def simpleArraySum(ar):
+    # Write your code here
+    return sum(ar)
+
+
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+print(simpleArraySum(ar))  # '''
+
+# 3/10 Compare the triplets
+'''a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+score_a = score_b = 0
+
+for i in range(len(a)):
+    score_a += a[i] > b[i]
+    score_b += b[i] > a[i]
+
+print(score_a, score_b  # '''
+
+
+# 4/10 A very big sum
+'''def sum_long_arr(ar):
+    return sum(ar)
+
+
+_, arr = input(), list(map(int, input().split()))
+print(sum_long_arr(arr))  # '''
+
+
+# 5/10 diagonal diference
+'''def diagonal_difference(ar):
+    primary_diagonal = sum(ar[i][i] for i in range(len(ar)))
+    secondary_diagonal = sum(ar[i][-i-1] for i in range(len(ar)))
+    # for i in range(len(ar)):
+    #     secondary_diagonal += ar[i][-i-1]
+    return abs(primary_diagonal - secondary_diagonal)
+
+
+n = int(input().strip())  # tamanho da matriz
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+print(diagonal_difference(arr))  # '''
+
+# 6/10 Plus Minus
+'''# quantidade(positivos) / len(arr)
+# quantidade(negativos) / "
+# quantidade(zeros) / "
+
+n, arr = int(input()), list(map(int, input().split()))
+positives = negatives = zeros = 0
+for i in arr:
+    positives += i > 0
+    negatives += i < 0
+    zeros += i == 0
+print(round(positives/n, 6))
+print(round(negatives/n, 6))
+print(round(zeros/n, 6))  # '''
+
+
+# 7/10 staircase '#'
+'''def staircase(n):    # Write your code here
+    for i in range(1, n+1):
+        print(f'{"#"*i:>{n}}')
+
+
+n = int(input().strip())
+staircase(n)  # '''
+
+
+# 8/10 Min-Max Sum
+'''def mini_max_sum(ar):
+    arr_sums = []
+    sum_arr = sum(ar)
+    for i in range(len(ar)):
+        arr_sums.append(sum_arr - ar[i])
+    print(min(arr_sums), max(arr_sums))
+
+
+arr = list(map(int, input().split()))
+mini_max_sum(arr)  # '''
+
+
+# 9/10 Birthday Cake Candles
+'''def birthday_cake_candles(candles):
+    return candles.count(max(candles))
+
+
+candles_count = int(input().strip())
+candles = list(map(int, input().rstrip().split()))
+print(birthday_cake_candles(candles))  # '''
+
+
+# 10/10 Time Conversion
+'''# 07:05:45PM -> 19:05:45
+def timeConversion(s):    # Write your code here
+    if s[-2:] == 'AM' and s[:2] == '12':
+        return '00' + s[2:-2]
+    elif s[-2:] == 'AM':
+        return s[:-2]
+    elif s[-2:] == "PM" and s[:2] == "12":
+        return s[:-2]
+    else:
+        return str(int(s[:2]) + 12) + s[2:8]
+
+
+s = input()
+print(timeConversion(s))  # '''
